2962dc56170661d2812528632abbae31013f7813
Nukually <317255300@qq.com>
2023-10-24 07:58:58 +0800

    feat:mac使用clang编译

--- build_mac.sh
+++ build_mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p build
 cd build
-cmake -G "Unix Makefiles" -DCMAKE_CXX_COMPILER=g++-10 -DCMAKE_C_COMPILER=gcc-10 ..
+cmake -G "Unix Makefiles" -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang ..
 make
 echo "Executable built and located in build/"
